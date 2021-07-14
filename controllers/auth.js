389fced8a2ea0b2378c1c1488e2b57b1a83f49da
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-07-13 23:42:26 -0400

    error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,13 +26,13 @@ const getLoggedInUser = async (req, res) => {
       let user = await User.findOne({email});
 
       if (!user) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({status_code : 400,success: false, msg: 'Invalid Credentials'});
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({status_code : 400,success: false,msg: 'Invalid Credentials'});
       }
 
       const payload = {
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = function(req, res, next) {
 
   // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ status_code : 401, success: false, msg: 'No token, authorization denied' });
   }
 
   try {
@@ -15,6 +15,6 @@ module.exports = function(req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
+    res.status(401).json({ status_code : 401, success: false, msg: 'Token is not valid' });
   }
 };
